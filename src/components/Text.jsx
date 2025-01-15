7af6847b559afadfbef9884c378d280e61eface5
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2025-01-15 16:21:58 +0200

    Exercise 10.11

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,11 +2,13 @@ import { Text as NativeText, StyleSheet } from 'react-native';
 import theme from '../../theme';
 
 const defaultStyle = StyleSheet.create({
-  color: theme.colors.text,
+  text: {
+    color: theme.colors.text,
+  },
 });
 
 const Text = ({ style, fontSize, fontWeight, color, ...props }) => {
-  const textStyle = [defaultStyle, style];
+  const textStyle = [defaultStyle.text, style];
   if (fontSize) textStyle.push({ fontSize });
   if (fontWeight) textStyle.push({ fontWeight });
   if (color) textStyle.push({ color });
